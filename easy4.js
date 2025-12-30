1b253f19cf4ad5624f78bbc41510b0a2975cb544
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-30 23:06:43 +0700

    Add files via upload

--- easy4.js
+++ easy4.js
@@ -1,11 +1,15 @@
-var lengthOfLastWord = function(string) {
-    list = []
-    list = string.trim().split(/\s+/)
-    return list[list.length - 1].length
-};
+var singleNumber = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+        }
+        if (count === 1) {
+            return nums[i];
+        }
+    }
+};  
 
-string = "aku cinta kamuu  "
-x = "   fly me   to   the moon"
-// console.log(x.split(/\s+/))
-console.log(lengthOfLastWord(string))
-console.log(lengthOfLastWord(x))
+console.log(singleNumber([4,1,2,1,2]));
