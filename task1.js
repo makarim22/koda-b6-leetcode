3f071be8afb8002116eb0a93d59c6a351d004f70
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-30 16:42:03 +0700

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,25 +1,30 @@
+// 2636 : apply transfrom over each element in array
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var map = function(arr, plusone, plusI, constant) {
+function plusone(item) { 
+    return item + 1;
+}
+
+function plusI(item, index) {
+    return item + index;
+}
+
+function constant(item) {
+    return 42;
+}
+
+var map = function(arr, callback) {
     let newArray = [];
     for(let i = 0; i < arr.length; i++){
-        newArray.push(plusI(arr[i], i));
-        newArray.push(plusone(arr[i]));
-        newArray.push(constant(arr[i]));
+        newArray.push(callback(arr[i], i));
     }
-
     return newArray;
-    function plusone(item) { 
-        return item +  1;
-    }
-    
-    function plusI (item, index) {
-        return item + index;
-    }
-
-    function constant(item) {
-        return 42;
-    }
 };
 
-console.log(map(array));
+
