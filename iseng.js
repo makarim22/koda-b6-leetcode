1f97aad9f813c5e2bc385b18a18a35fec062db7c
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-30 16:42:23 +0700

    Add files via upload

---
+++ iseng.js
@@ -0,0 +1,27 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+function greaterThan(item){
+    return item > 10; 
+}
+
+function firstIndex(item, index){
+    return index === 0; 
+}
+
+function plusOne(item){
+    return item + 1; 
+}
+
+var filter = function(arr, fn) {
+    let newArray = [];
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i], i)) { 
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+};
+
+console.log(filter(array, greaterThan));  
+console.log(filter(array, firstIndex));  
+console.log(filter(array, plusOne));       
